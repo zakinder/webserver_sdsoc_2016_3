2c1273e0b374cc28a8b5a62d3e2d520e15f72ec7
sakinder <sakinder@live.com>
2017-05-09 04:20:43 -0500

    Init_rev_0
    files uploads

---
+++ design_basic_settings_set.cmd
@@ -0,0 +1,9 @@
+@set XILDIR=C:/Xilinx/SDx
+@set VIVADO_VERSION=atm.umd
+@set PARTNUMBER=5
+@set SWAPP=NA
+@set PROGRAM_ROOT_FOLDER_FILE=1
+@set DO_NOT_CLOSE_SHELL=0
+@set ZIP_PATH=D:/SVN/cores/zip/zip.exe
+@set ENABLE_SDSOC=1
+@set XILINXGIT_DEVICETREE=D:/xilinx_git/device-tree-xlnx
